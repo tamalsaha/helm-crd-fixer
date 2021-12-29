97eaee6954e64522e3749e47a32257136355e758
Tamal Saha <tamal@appscode.com>
2021-12-29 10:39:46 -0800

    added-all
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- main.go
+++ main.go
@@ -1,33 +1,14 @@
 package main
 
 import (
-	"context"
-	"fmt"
-	"k8s.io/apiextensions-apiserver/pkg/apis/apiextensions"
+	crd_cs "k8s.io/apiextensions-apiserver/pkg/client/clientset/clientset"
 	"k8s.io/klog/v2"
-
-	//	"time"
-
-	// 	"github.com/nats-io/nats.go"
-	// 	"github.com/tamalsaha/nats-hop-demo/shared"
-	// 	"github.com/tamalsaha/nats-hop-demo/transport"
-	core "k8s.io/api/core/v1"
-	"k8s.io/apimachinery/pkg/runtime"
-	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
-	//	"k8s.io/klog/v2"
 	"k8s.io/klog/v2/klogr"
 	"kmodules.xyz/client-go/apiextensions"
 	appcat "kmodules.xyz/custom-resources/apis/appcatalog/v1alpha1"
 	catalogapi "kubedb.dev/apimachinery/apis/catalog/v1alpha1"
 	dbapi "kubedb.dev/apimachinery/apis/kubedb/v1alpha2"
 	ctrl "sigs.k8s.io/controller-runtime"
-	"sigs.k8s.io/controller-runtime/pkg/client"
-	"sigs.k8s.io/controller-runtime/pkg/client/apiutil"
-)
-
-var (
-	scheme   = runtime.NewScheme()
-	setupLog = ctrl.Log.WithName("setup")
 )
 
 func main() {
@@ -37,56 +18,18 @@ func main() {
 }
 
 func run() error {
-	_ = clientgoscheme.AddToScheme(scheme)
-
-	// 	nc, err := nats.Connect(shared.NATS_URL)
-	// 	if err != nil {
-	// 		klog.Fatalln(err)
-	// 	}
-	// 	defer nc.Close()
-
 	ctrl.SetLogger(klogr.New())
 	cfg := ctrl.GetConfigOrDie()
 
-	// 	tr, err := cfg.TransportConfig()
-	// 	if err != nil {
-	// 		panic(err)
-	// 	}
-	// 	cfg.Transport, err = transport.New(tr, nc, "k8s", 10000*time.Second)
-	// 	if err != nil {
-	// 		panic(err)
-	// 	}
-
-	mapper, err := apiutil.NewDynamicRESTMapper(cfg)
+	crdClient, err := crd_cs.NewForConfig(cfg)
 	if err != nil {
 		return err
 	}
-
-	c, err := client.New(cfg, client.Options{
-		Scheme: scheme,
-		Mapper: mapper,
-		Opts: client.WarningHandlerOptions{
-			SuppressWarnings:   false,
-			AllowDuplicateLogs: false,
-		},
-	})
-	if err != nil {
-		return err
-	}
-
-	var nodes core.NodeList
-	err = c.List(context.TODO(), &nodes)
-	if err != nil {
-		panic(err)
-	}
-	for _, n := range nodes.Items {
-		fmt.Println(n.Name)
-	}
-	return nil
+	return EnsureCustomResourceDefinitions(crdClient)
 }
 
 // EnsureCustomResourceDefinitions ensures CRD for MySQl, DormantDatabase and Snapshot
-func EnsureCustomResourceDefinitions() error {
+func EnsureCustomResourceDefinitions(crdClient crd_cs.Interface) error {
 	klog.Infoln("Ensuring CustomResourceDefinition...")
 	crds := []*apiextensions.CustomResourceDefinition{
 		dbapi.Elasticsearch{}.CustomResourceDefinition(),
@@ -116,5 +59,5 @@ func EnsureCustomResourceDefinitions() error {
 
 		appcat.AppBinding{}.CustomResourceDefinition(),
 	}
-	return apiextensions.RegisterCRDs(c.CRDClient, crds)
+	return apiextensions.RegisterCRDs(crdClient, crds)
 }
